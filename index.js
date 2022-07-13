b516104beadc10b47cc4be18fe5220b60781a4d8
jay <jayesh10051@gmail.com>
2022-07-13 17:39:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+for (let index = 0; index < document.querySelectorAll(".drum").length; index++) {
+    document.querySelectorAll("button.drum")[index].addEventListener("click", function run() {
+       
+        makeSound(this.textContent);
+        buttonAnimation(this.textContent);
+    })};
+    document.addEventListener("keydown", function (event) {
+        makeSound(event.key);
+        buttonAnimation(event.key);
+    });
+    function makeSound(key) {
+        switch (key) {
+            case "w":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case "a":
+                var kickBass = new Audio("sounds/kick-bass.mp3");
+                kickBass.play();
+                break;
+            case "s":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            case "d":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "j":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "k":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "l":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+
+        }
+    }
+function buttonAnimation(currentKey) {
+     var k=document.querySelector("."+currentKey);
+     k.classList.add("pressed");
+
+     setTimeout(() => {
+         k.classList.remove("pressed");
+     },50);
+    
+    }
